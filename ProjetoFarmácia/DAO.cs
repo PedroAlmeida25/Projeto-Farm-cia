ef1c958b97bfee1398204e91a090d033d8126ccd
PEDRO AUGUSTO SANTOS ALMEIDA <pedro.asalmeida1@senacsp.edu.br>
2024-02-20 22:16:47 -0300

    Primeiro Comando

---
+++ ProjetoFarmácia/DAO.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ProjetoFarmácia
+{
+    class DAO
+    {
+        MySqlConnection conexao;
+        public string dados;
+        public string sql;
+        public int[] codigo;
+        public string[] nome;
+        public string[] telefone;
+        public string[] endereco;
+        public string[] rg;
+        public int i;
+        public int contador;
+
+        public DAO()
+        {
+            conexao = new MySqlConnection("server=localhost;DataBase=ProjetoFarmacia;Uid=root;Password=");
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Conectado com sucesso!");//Temporariamente             
+            }//fim do try
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro + "\n\nAlgo deu errado!");
+                conexao.Close();
+            }//fim do catch
+        }//fim do construtor
+
+        public void Inserir(int codigo, string nome, string telefone, string endereco, string rg)
+        {
+            dados = "('" + codigo + "','" + nome + "','" + telefone + "','" + endereco + "','" + rg + "')";
+            sql = "insert into cliente(codigo, nome, telefone, endereço, rg) values" + dados;
+
+            try
+            {
+                MySqlCommand conn = new MySqlCommand(sql, conexao);
+                MessageBox.Show(conn.ExecuteNonQuery() + " dado inserido!");
+            }//fim do try
+            catch (Exception erro)
+            {
+                MessageBox.Show("Algo deu errado!\n\n" + erro);
+            }//fim do catch
+        }//fim do método
+
+        public void PreencherVetor()
+        {
+            string query = "select * from cliente";
+
+            //Instanciar os vetores
+            this.codigo = new int[100];
+            this.nome = new string[100];
+            this.telefone = new string[100];
+            this.endereco = new string[100];
+            this.rg = new string[100];
+
+            //Preencher com valores genéricos
+            for (i = 0; i < 100; i++)
+            {
+                codigo[i] = 0;
+                nome[i] = "";
+                telefone[i] = "";
+                endereco[i] = "";
+                rg[i] = "";
+            }//fim do for
+
+            //Criar o comando de consultar no BD
+            MySqlCommand coletar = new MySqlCommand(query, conexao);
+            //Listar todos os dados que estão no banco
+            MySqlDataReader leitura = coletar.ExecuteReader();
+
+            i = 0;//Utilizar novamente o contar i
+            contador = 0;//Contar quantos dados eu tenho no banco
+            while (leitura.Read())
+            {
+                codigo[i] = Convert.ToInt32(leitura["codigo"]);
+                nome[i] = "" + leitura["nome"];
+                telefone[i] = "" + leitura["telefone"];
+                endereco[i] = "" + leitura["endereço"];
+                rg[i] = "" + leitura["rg"];
+                i++;//Mudando o contador
+                contador++;//Contar quantos dados tem no banco
+            }//fim do while
+
+            //Encerrar o banco
+            leitura.Close();
+        }//fim do método
+    }//fim da classe
+}//fim da DAO
---
+++ ProjetoFarmácia/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoFarmácia
+{
+    public partial class Form1 : Form
+    {
+        DAO bd;
+        public int codigo;
+        public string nome;
+        public string telefone;
+        public string endereco;
+        public string Rg;
+        public Form1()
+        {
+            InitializeComponent();
+            bd = new DAO();//Conectar a tela com a classe DAO
+        }//fim do método construtor
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }//faz o carregamento de conteúdos da minha tela
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//fim do código
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//fim do nome
+
+        private void maskedTextBox3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//fim do telefone
+
+        private void maskedTextBox4_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//fim do endereço
+
+        private void maskedTextBox5_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//fim do rg
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cod = Convert.ToInt32(codigo);
+            string name = nome;
+            string tel = telefone;
+            string end = endereco;
+            string rg = Rg;
+
+            bd.Inserir(cod, name, tel, end, rg);//Inserindo no BD
+
+            //Limpando os campos
+            codigo = 0;
+            nome = "";
+            telefone = "";
+            endereco = "";
+            Rg = "";
+        }//fim do botão cadastrar
+    }//fim da classe
+}//fim do projeto
